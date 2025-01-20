03a2abe41dd3afa683088b7dcb3882f2210905a8
Sierra <sierra.ar.sager@gmail.com>
2025-01-19 20:54:18 -0500

    completed assignment

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,41 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+function findSmallestNumber(array) {
+    let smallestNumber = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < smallestNumber) {
+            smallestNumber = array[i];
+        }
+    }
+    return smallestNumber;
+}
+
+function findLargestNumber(array) {
+    let largestNumber = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > largestNumber) {
+            largestNumber = array[i];
+        }
+    }
+    return largestNumber;
+}
+
+function findAverage(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
+}
+
+function render () {
+    smallestNumberElement.innerText = findSmallestNumber(myNumbers);
+    largestNumberElement.innerText = findLargestNumber(myNumbers);
+    averageNumberElement.innerText = findAverage(myNumbers);
+}
+
+submissionBtn.addEventListener("click", function () {
+    render();
+  });
